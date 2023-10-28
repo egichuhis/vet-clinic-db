0ef1b443eb5c94af301b5cc840bf8192fe63000c
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-10-28 11:47:58 +0300

    Add queries

--- schema.sql
+++ schema.sql
@@ -20,5 +20,32 @@ CREATE TABLE owners
     age INTEGER
 );
 
+CREATE TABLE species
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+-- Drop the existing foreign key constraints if they exist
+ALTER TABLE animals DROP CONSTRAINT IF EXISTS fk_species;
+ALTER TABLE animals DROP CONSTRAINT IF EXISTS fk_owner;
+
+-- Drop the existing column species
+ALTER TABLE animals DROP COLUMN IF EXISTS species;
+
+-- Add the column species_id as a foreign key referencing the species table
+ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+-- Add the column owner_id as a foreign key referencing the owners table
+ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+ALTER TABLE animals 
+ADD COLUMN species_id INTEGER;
+
+ALTER TABLE animals 
+ADD COLUMN owner_id INTEGER;
+
 
 
